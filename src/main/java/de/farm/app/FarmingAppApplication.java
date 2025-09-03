9dbf7221188edfe40c7088b7b3635705df410959
StephaneWafo <stephanekechi@gmail.com>
2025-09-03 12:00:30 +0200

    add maven project

---
+++ src/main/java/de/farm/app/FarmingAppApplication.java
@@ -0,0 +1,13 @@
+package de.farm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/farm/app/FarmingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.farm.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
